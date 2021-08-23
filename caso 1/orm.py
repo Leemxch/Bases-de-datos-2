826198711955e669d5c1df24ddb6e793ed986bf0
Leemxch <61258635+Leemxch@users.noreply.github.com>
2021-08-23 16:20:43 -0600

    Try-except

--- caso 1/orm.py
+++ caso 1/orm.py
@@ -2,13 +2,14 @@
 # 2018093728 - Paula Mariana Bustos Vargas
 # 2019185076 - Max Richard Lee Chung
 
-''' -------------------------------------------------------------------------------------------------
-                                         Caso 1
------------------------------------------------------------------------------------------------------ '''
+#-------------------------------------------------------------------------------------------------
+#                                         Caso 1
+#-------------------------------------------------------------------------------------------------
 
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import exc
 
 #Coneccion con la base de datos ('mssql+pyodbc://server/databsae?driver=SQL+Server+Native+Client+11.0')
 #Echo sirve para visualizar las ejecuciones que realiza
@@ -23,7 +24,7 @@ class solutionsLog(Base):
     __tablename__ = 'sd_solutionslog'
     solutionlogid = sa.Column('solutionlogid', sa.Integer(),primary_key = True)
     posttime =  sa.Column('posttime', sa.DateTime() , nullable=False)
-    actiontypeid = sa.Column('actiontypeid', sa.Integer(), nullable=False)
+    actiontypeid = sa.Column('actiontypeid', sa.Integer(), sa.ForeignKey('action.actiontypeid'))
     solutionid = sa.Column('solutionid', sa.Integer(), nullable=False)
 
 #Clase action, representacion de la tabla sd_solutionslog
@@ -33,9 +34,9 @@ class action(Base):
     name = sa.Column('name', sa.String(255), nullable=False)
     iconurl = sa.Column('iconurl', sa.String(255), nullable=False)
 
-''' -------------------------------------------------------------------------------------------------
------------------------------------------  Relacion 1 a N   -----------------------------------------
-- ------------------------------------------------------------------------------------------------- '''
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------  Relacion 1 a N   -----------------------------------------
+#-------------------------------------------------------------------------------------------------
 
 #Tablas a usar: solutionsLog, action
 #Se declaran las varaibles tablas cuya utilidad es para poder acceder a las clases instanciadas
@@ -66,9 +67,9 @@ for i in test:
     print(i)
 '''
 
-''' -------------------------------------------------------------------------------------------------
------------------------------------------  Object Pooling   -----------------------------------------
-- ------------------------------------------------------------------------------------------------- '''
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------  Object Pooling   -----------------------------------------
+#-------------------------------------------------------------------------------------------------
 
 engine = sa.create_engine('mssql+pyodbc://(LocalDb)\MSSQLLocalDB/solutiondesigns?driver=SQL+Server+Native+Client+11.0', 
                             echo = False,
@@ -81,7 +82,7 @@ engine = sa.create_engine('mssql+pyodbc://(LocalDb)\MSSQLLocalDB/solutiondesigns
                             # Cuando la cantidad de conexiones sobrepasa la capacidad maxima del pool_size, se mantienen en espera, sin embargo, si la cantidad de conexiones sobrepasan el max_overflow, se desconectaran automaticamente
                             max_overflow = 3, 
                           
-                   ''' número total de conexiones simultáneas = pool_size + max_overflow '''
+                            #número total de conexiones simultáneas = pool_size + max_overflow
 
                             # Es una funcion propia Pool. El valor predeterminado es -1. 
                             # El numero de segundos entre el reciclaje de la conexión, lo que significa que al finalizar el "checkout", si se supera este tiempo de espera, la conexión se cerrará y se reemplazará por una conexión recién abierta
@@ -93,22 +94,26 @@ engine = sa.create_engine('mssql+pyodbc://(LocalDb)\MSSQLLocalDB/solutiondesigns
                         )
 
 
-''' -------------------------------------------------------------------------------------------------
-------------------------------  Transaccion que afecta a más de una tabla  --------------------------
-- ------------------------------------------------------------------------------------------------- '''
+#-------------------------------------------------------------------------------------------------
+#-------------------------  Transaccion que afecta a más de una tabla  ---------------------------
+#-------------------------------------------------------------------------------------------------
 
 #Creamos la sesion para la transaccion virtual enlazada al engine con la configuracion del object pooling
 Session = sessionmaker(bind=engine)
 
 #Variable para mapear la insercion de datos a las tablas, este caso se da por la columna nombre(name)
-nombre = 'pez18_prueba'
+nombre = 'pez40_prueba'
 
 #Declaramos la instancia de la sesion con la configuracion del engine 
 ses = Session()
-#Hacemos insert a la tabla action y luego insert a la tabla solutions mapeando por medio del nombre
-ses.add(action(name = nombre ,iconurl = 'some url'))
-#El actiontypeid se consigue por medio de un subquery, donde se obtienen las filas con la condicion de que tengan el mismo nombre,
-#luego se selecciona la primera fila y, como el resultado es una fila de una tabla, se saca el dato bajo el nombre de actiontypeid
-ses.add(solutionsLog(posttime = '2021-08-22', actiontypeid = ses.query(actions).where(actions.columns.name == nombre).first().actiontypeid, solutionid = 4))
-#Si no hubo error, se le hace commit y, si hay error, no se hace commit
-ses.commit()
+try:
+    #Hacemos insert a la tabla action y luego insert a la tabla solutions mapeando por medio del nombre
+    ses.add(action(name = nombre ,iconurl = 'some url'))
+    #El actiontypeid se consigue por medio de un subquery, donde se obtienen las filas con la condicion de que tengan el mismo nombre,
+    #luego se selecciona la primera fila y, como el resultado es una fila de una tabla, se saca el dato bajo el nombre de actiontypeid
+    ses.add(solutionsLog(posttime = '2021-08-22', actiontypeid = ses.query(actions).where(actions.columns.name == nombre).first().actiontypeid, solutionid = 4))
+    #Si no hubo error, se le hace commit y, si hay error, no se hace commit
+    ses.commit()
+except exc.SQLAlchemyError:
+    ses.rollback()
+    
